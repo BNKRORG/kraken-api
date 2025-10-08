6cebdf173b6ddf10b044870a7efd84abcc01bb0b
Yuki Kishimoto <yukikishimoto@protonmail.com>
2025-10-08 14:52:58 -0300

    Add example for fetching Kraken account balances

---
+++ examples/balances.rs
@@ -0,0 +1,20 @@
+use kraken_api::auth::KrakenAuth;
+use kraken_api::client::KrakenClient;
+
+#[tokio::main]
+async fn main() {
+    let auth = KrakenAuth::api_keys("<api-key>", "<secret>");
+
+    let client = KrakenClient::new(auth).unwrap();
+
+    let balances = client.balances().await.unwrap();
+
+    if balances.is_empty() {
+        println!("No balances");
+        return;
+    }
+
+    for (coin, amount) in balances {
+        println!("{coin}: {amount}");
+    }
+}
