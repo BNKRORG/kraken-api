bab6d4a969171b23aee2379d04d4203ac28a439c
Yuki Kishimoto <yukikishimoto@protonmail.com>
2025-12-04 11:07:44 +0100

    Update `USER_AGENT_NAME` to use `CARGO_PKG_NAME`
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

--- src/constant.rs
+++ src/constant.rs
@@ -7,7 +7,9 @@ pub(crate) const API_ROOT_URL: &str = "https://api.kraken.com";
 
 pub(crate) const API_VERSION: u16 = 0;
 
-pub(crate) const USER_AGENT_NAME: &str = concat!("kraken-api/", env!("CARGO_PKG_VERSION"));
+/// User Agent for the client
+pub(super) const USER_AGENT_NAME: &str =
+    concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
 
 /// Kraken BTC ticker symbol
 pub const XBT_TICKER: &str = "XBT";
